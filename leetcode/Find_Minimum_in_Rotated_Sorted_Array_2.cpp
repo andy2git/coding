cefc990b4f81d6221777f707d9934730c0ec3915
andy2hub <andy.cj.wu@gmail.com>
2014-12-11 14:26:34 -0800

    better solution

--- leetcode/Find_Minimum_in_Rotated_Sorted_Array_2.cpp
+++ leetcode/Find_Minimum_in_Rotated_Sorted_Array_2.cpp
@@ -1,30 +1,17 @@
 class Solution {
 public:
     int findMin(vector<int> &num) {
-        if(num.empty()) throw runtime_error("empty vector!");
         if(num.size() == 1) return num[0];
         
-        size_t l = 0;
-        size_t h = num.size()-1;
-        size_t m;
-        
+        int l = 0;
+        int h = num.size() -1;
         while(l < h){
             if(l+1 == h) return min(num[l], num[h]);
             
-            m = l + (h-l)/2;
-            if(num[m] < num[h]){
-                h = m;  
-
-            // TODO: needs to be improved
-            } else if(num[l] == num[m]){
-                while(l < m && num[l] == num[m]) l++;
-            } else if(num[h] == num[m]){
-                while(h > m && num[h] == num[m]) h--;
-            }else{
-                l = m;
-            }
+            int m = l + (h-l)/2;
+            if(num[m] < num[h]) h = m;
+            else if (num[m] == num[h]) h--;
+            else l = m;
         }
-        
-        if(l == h) return num[l];
     }
 };
