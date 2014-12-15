0b0c14d8b167c185df9aa3182a7879ef284409a1
andy2hub <andy.cj.wu@gmail.com>
2014-12-15 09:50:46 -0800

    improvment

--- leetcode/Word_Break.cpp
+++ leetcode/Word_Break.cpp
@@ -1,30 +1,36 @@
+/**
+ * Another solution is to keep track of all possible s[i->j] in tbl[i][j]
+ *
+ * Memoization solution: 
+ *    - m[i]: work breakable starting from i to the end of the string s
+ * 
+ */
 class Solution {
 public:
     bool wordBreak(string s, unordered_set<string> &dict) {
-        if(s == "") return true;
+        if(s.empty()) return true;
+        if(dict.find(s) != dict.end()) return true;
         
-        // -1: init. 0: false, 1: true
-        vector<int> A(s.size()+1, -1);
-        A[s.size()] = 1;
+        vector<int> m(s.length(), -1);
         
-        return word_break_helper(s, dict, 0, A) > 0;
+        m[0] = wb_helper(s, 0, dict, m);
         
+        return m[0] > 0;
     }
 private:
-    //b is the start index for the new string s, it is used to mark the index for A
-    int word_break_helper(string s, unordered_set<string> &dict, int b, vector<int> &A){
-        if(s == "") return 1;
+    int wb_helper(string &s, int sInd, unordered_set<string> &dict, vector<int> &m){
+        if(sInd >= s.length()) return 1;
+        if(dict.find(s.substr(sInd)) != dict.end()) {
+            m[sInd] = 1;
+            return 1;
+        }
         
-        for(int i = 1; i <= s.size(); i++){
-            if(dict.find(s.substr(0, i)) != dict.end()){
-                if(A[b+i] == -1){
-                    A[b+i] = word_break_helper(s.substr(i), dict, b+i, A);
+        for(int i = sInd; i < s.length(); i++){
+            if(dict.find(s.substr(sInd, i-sInd+1)) != dict.end()){
+                if(m[i+1] == -1) {
+                    m[i+1] = wb_helper(s, i+1, dict, m);
                 }
-                
-                // pre-cached value for A[i]
-                if(A[b+i] == 1){
-                    return 1;
-                }
+                if(m[i+1] == 1) return 1;
             }
         }
         
