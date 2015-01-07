4469eb9daf44b4dfb749d305644efe282d2f49ca
andy2hub <andy.cj.wu@gmail.com>
2015-01-06 21:38:04 -0800

    more solution

--- leetcode/Find_Peak_Element.cpp
+++ leetcode/Find_Peak_Element.cpp
@@ -4,20 +4,21 @@ public:
     int findPeakElement(const vector<int> &num) {
         if(num.size() == 1) return 0;
         
-        vector<int> A;
-        A.push_back(INT_MIN);
-        for(int i = 0; i < num.size(); i++) A.push_back(num[i]);
-        A.push_back(INT_MIN);
+        vector<int> A(num.size()+2);
+        A[0] = INT_MIN;
+        copy(num.begin(), num.end(), A.begin()+1);
+        A[A.size()-1] = INT_MIN;
         
         int l = 0;
         int h = A.size()-1;
         int m;
-        while(l+1 < h){
+        while(l <= h){
             m = l + (h-l)/2;
-            if(A[m] > A[m-1] && A[m] > A[m+1]) return m-1;
-            if(A[m] < A[m-1]) h = m;
-            else l = m;
+            if(A[m] > A[m+1] && A[m] > A[m-1]) return m-1;
+            if(A[m] < A[m+1]) l = m+1;
+            else h = m-1;
         }
+        return -1;
     }
 };
 
